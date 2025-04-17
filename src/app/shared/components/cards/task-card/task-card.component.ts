93b886ce082cbf0b202470d9c933e204bc6fcb11
Gabriel Ferreira <gabrielcarfepro@gmail.com>
2025-04-17 10:35:12 -0300

    Removed the css style for button-wrapper at task-card.

--- src/app/shared/components/cards/task-card/task-card.component.ts
+++ src/app/shared/components/cards/task-card/task-card.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { CircleButtonComponent } from '../../buttons/circle-button/circle-button.component';
 
 @Component({
   selector: 'app-task-card',
   standalone: true,
-  imports: [],
+  imports: [CircleButtonComponent],
   templateUrl: './task-card.component.html',
   styleUrl: './task-card.component.scss'
 })

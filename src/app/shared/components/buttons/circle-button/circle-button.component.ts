85e1dd481ac51dee12c766fe3642d415ac3848fe
Gabriel Ferreira <gabrielcarfepro@gmail.com>
2025-04-17 10:33:34 -0300

    Created the circle-button component for code reuse.

---
+++ src/app/shared/components/buttons/circle-button/circle-button.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-circle-button',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './circle-button.component.html',
+  styleUrl: './circle-button.component.scss'
+})
+export class CircleButtonComponent {
+
+}

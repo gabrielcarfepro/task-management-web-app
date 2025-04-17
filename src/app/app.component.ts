ca71f88ede4db758d11d86e4d75934b0c64c0488
Gabriel Ferreira <gabrielcarfepro@gmail.com>
2025-04-17 10:29:29 -0300

    task-card imported at app.component to see implementation.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TaskCardComponent } from './shared/components/cards/task-card/task-card.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TaskCardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
